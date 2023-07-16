7806b7fef9e381051f3cdc166c63014f96077f8c
stepan <stepan.anfinogenov@gmail.com>
2023-07-16 11:42:30 +0100

    Commit to save in github repo

---
+++ src/main/java/com/anf2/lesson7/App.java
@@ -0,0 +1,13 @@
+package com.anf2.lesson7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/anf2/lesson8/Lesson8.java
@@ -0,0 +1,5 @@
+package com.anf2.lesson8;
+
+
+public class Lesson8 {
+}
